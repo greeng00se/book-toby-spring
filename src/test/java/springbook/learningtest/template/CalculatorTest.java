eb9fb6dd72d0c6d365cc25914069e17d6be29a78
greeng00se <thegreengoose7@gmail.com>
2022-03-17 10:03:25 +0900

    템플릿/콜백 패턴 계산기 예제

---
+++ src/test/java/springbook/learningtest/template/CalculatorTest.java
@@ -0,0 +1,28 @@
+package springbook.learningtest.template;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class CalculatorTest {
+
+    Calculator calculator;
+    String numFilePath;
+
+    @BeforeEach
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilePath = getClass().getResource("/numbers.txt").getPath();
+    }
+    @Test
+    void sumOfNumbers() throws IOException {
+        Assertions.assertThat(calculator.calcSum(this.numFilePath)).isEqualTo(10);
+    }
+
+    @Test
+    void multiplyOfNumbers() throws IOException {
+        Assertions.assertThat(calculator.calcMultiply(this.numFilePath)).isEqualTo(24);
+    }
+}
