bd5f494c20d0755b3b3812bea7581d3f65b68222
greeng00se <thegreengoose7@gmail.com>
2022-03-31 22:16:10 +0900

    스프링의 프록시 팩토리 빈

---
+++ src/test/java/springbook/learningtest/proxy/DynamicProxyTest.java
@@ -0,0 +1,47 @@
+package springbook.learningtest.proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import java.lang.reflect.Proxy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DynamicProxyTest {
+
+    @Test
+    void simpleProxy() {
+        Hello dynamicProxyHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[] { Hello.class },
+                new UppercaseHandler(new HelloTarget())
+        );
+
+        assertThat(dynamicProxyHello.sayHello("GOOSE")).isEqualTo("HELLO GOOSE");
+        assertThat(dynamicProxyHello.sayHi("GOOSE")).isEqualTo("HI GOOSE");
+        assertThat(dynamicProxyHello.sayThankYou("GOOSE")).isEqualTo("THANK YOU GOOSE");
+    }
+
+    @Test
+    void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice());
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+        assertThat(proxiedHello.sayHello("GOOSE")).isEqualTo("HELLO GOOSE");
+        assertThat(proxiedHello.sayHi("GOOSE")).isEqualTo("HI GOOSE");
+        assertThat(proxiedHello.sayThankYou("GOOSE")).isEqualTo("THANK YOU GOOSE");
+    }
+
+    private class UppercaseAdvice implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            String ret = (String) invocation.proceed();
+            return ret.toUpperCase();
+        }
+    }
+}
